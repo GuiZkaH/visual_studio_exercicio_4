ea25fa3a866f35027808e8636d249116b6554b94
GuiZkaH <52414162+GuiZkaH@users.noreply.github.com>
2020-03-18 14:26:21 +0000

    Add files via upload

---
+++ Projeto_Soma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	int soma;
+
+    cout << "Primeiro numero: ";
+	cin >> num1;
+	cout << "Segundo numero: ";
+	cin >> num2;
+	soma = num1 + num2;
+
+	cout << "A soma destes valores e de " << soma;
+}
